2b8a7f9000ab52e8b923da2fde9e952c3194d7fd
Arun Bharath Krishnan <arunbharath.k@gmail.com>
2016-11-14 16:20:21 -0800

    first commit

---
+++ Assets/_scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    static Animator anim;
+	// Use this for initialization
+	void Start () {
+
+        anim.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("isJumping");
+        }
+	
+	}
+}
